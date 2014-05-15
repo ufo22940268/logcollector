6f3555f2ce1992bccdf81086563d889b11ef7831
ufo22940268 <ufo22940268@gmail.com>
2014-05-15 15:36:19 +0800

    修改主题

--- app/src/main/java/me/biubiubiu/logcollector/app/MainActivity.java
+++ app/src/main/java/me/biubiubiu/logcollector/app/MainActivity.java
@@ -1,32 +1,10 @@
 package me.biubiubiu.logcollector.app;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.app.Activity;
 import android.os.Bundle;
-import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-import me.biubiubiu.logcollector.app.ui.LogView;
-import me.biubiubiu.logcollector.app.util.SystemManager;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
